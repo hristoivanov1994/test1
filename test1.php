f6c68641f4bda39a6a9de2d07468372e650cba20
Hristo Ivanov <hristo_ivanov@abv.bg>
2017-07-01 20:35:40 +0300

    returning to master branch

--- test1.php
+++ test1.php
@@ -10,7 +10,14 @@ public function __get($name)
     // TODO: Implement __get() method.
     return $this->tester;
 }
+public function setSomething()
+{
 
 }
+public function getSomething()
+{
 
+}
+}
+$test = new test();
 
